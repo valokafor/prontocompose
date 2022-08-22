ffc4de01efc93326d522cdd71b3584ac7c7215fc
Val Okafor <val@okason.com>
2022-08-22 10:35:06 -0700

    End day 2 - add column to Pronto Resume Screens

---
+++ ProntoResume/app/src/main/java/com/prontocompose/prontoresume/ProntoResumeApp.kt
@@ -0,0 +1,11 @@
+package com.prontocompose.prontoresume
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.prontocompose.prontoresume.screens.ProfileScreen
+import com.prontocompose.prontoresume.screens.ProjectsScreen
+
+@Composable
+fun ProntoResumeApp() {
+    ProfileScreen()
+}
